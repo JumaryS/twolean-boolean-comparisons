32ecb745eb4b3c47b95f539f9951b85181c432ca
JumarySanchez <sanchezjumary@gmail.com>
2020-06-17 09:45:53 -0400

    booleans

--- main.js
+++ main.js
@@ -1,7 +1,50 @@
 /********************
  * YOUR CODE BELOW! *
  ********************/
+function moreThan5(num) {
+  return num>5;
+}
+function topScore(num1,num2) {
+  return num1>num2;
+}
+function isInDanger(grade) {
+  return grade>=60 && grade<=71;
+}
+function isCoasting(grade) {
+  return 71<grade && 83>=grade;
+}
+function isSucceeding(grade) {
+  return grade>=84 && grade<=92;
+}
+function isFailing(grade) {
+  return grade<60;
+}
+function isAcing(grade) {
+  return grade>92;
+}
+function isStudent(student) {
+  return student === 'student'
+}
+function isTeacher(teacher) {
+  return teacher === 'teacher'
+}
+function isAdmin(admin) {
+  return admin === 'admin'
+}
+function isElementary(schoolLevel) {
+  return schoolLevel === 'elementary'
+}
 
+function areDifferentPeople(jumi1,jumi2){
+// return jumi1 === jumi2 || jumi1 !== jumi2
+return jumi1 !== jumi2
+}
+function notAnElementarySchoolAdministrator(role, level) {
+  return role !== 'elementary' || level !== 'admin'
+}
+function isMiddleSchoolTeacher(role, level) {
+  return role === 'teacher' && level>=6 && level<=8;
+}
 
 
 
